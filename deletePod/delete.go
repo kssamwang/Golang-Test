c7935f513bbb5a6b228a1f61c172c94fce324bc2
root <root@localhost.localdomain>
2023-05-09 22:12:04 +0800

    update delete pod

--- deletePod/delete.go
+++ deletePod/delete.go
@@ -3,46 +3,64 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 	"path/filepath"
 	"flag"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
 
-func deletePod(podName string, namespace string) {
-	// 连接集群
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
+func initClientset() (*kubernetes.Clientset,error){
+		// 连接集群
+		var kubeconfig *string
+		if home := homedir.HomeDir(); home != "" {
+				kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+		} else {
+				kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+		}
+		flag.Parse()
 
-	// 使用kubeconfig中的当前上下文,加载配置文件
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		panic(err.Error())
-	}
-	// 创建clientset
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
+		// 使用kubeconfig中的当前上下文,加载配置文件
+		config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+		if err != nil {
+				panic(err.Error())
+		}
+		// 创建clientset
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+				panic(err.Error())
+		}
+	return clientset,err
+}
 
-	// 删除pod
+func deletePod(clientset *kubernetes.Clientset, podName string, namespace string) {
 	err := clientset.CoreV1().Pods(namespace).Delete(context.TODO(), podName, metav1.DeleteOptions{})
 	if err != nil {
-		log.Fatal(err)
+		panic(err.Error())
+	} else {
+		fmt.Printf("Start to delete pod %q in namespace %q and wait for terminating...\n", podName, namespace)
+		for {
+			pod, e := clientset.CoreV1().Pods(namespace).Get(context.TODO(),podName, metav1.GetOptions{})
+			if e != nil{
+				break
+			} else if pod.Status.Phase == "Terminating" {
+				time.Sleep(1 * time.Second)
+			}
+		}
+		fmt.Printf("Pod %q in namespace %q has been deleted safely.\n", podName, namespace)
 	}
-
-	fmt.Printf("Delete pod %q in namespace %q.\n", podName, namespace)
 }
 
 func main() {
-	deletePod("gpu-pod1","default")
-	deletePod("gpu-pod2","default")
+	clientset,err := initClientset()
+	if err != nil {
+				panic(err.Error())
+		return
+		}
+	deletePod(clientset,"gpu-pod1","default")
+	deletePod(clientset,"gpu-pod2","default")
 }
+
+
