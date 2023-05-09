a04c28e8ef3a3e82ef748c1e72ee3a3442421f01
root <root@localhost.localdomain>
2023-05-10 02:02:35 +0800

    update Environment

--- setEnv/setenv.sh
+++ setEnv/setenv.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+sudo echo -e "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
+source /etc/profile
+go version
+
 go env -w GOPROXY=https://mirrors.aliyun.com/goproxy/
 go env -w GOPROXY=https://goproxy.cn,direct
 go env -w GOSUMDB=off
