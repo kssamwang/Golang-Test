c58786ed81ef964cefb94537679a015952987411
root <root@localhost.localdomain>
2023-05-10 02:53:04 +0800

    update Get Node Info

--- nodeInfo/getNodeInfo.go
+++ nodeInfo/getNodeInfo.go
@@ -5,12 +5,34 @@ import (
 	"fmt"
 	"flag"
 	"path/filepath"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
+
+func printNodeInfo(node *corev1.Node) {
+	if node == nil{
+		return
+	}
+	fmt.Println("============================================================================")
+	fmt.Println("Node Name                : ",node.Name)
+	fmt.Println("Node Status              : ",node.Status.Conditions[len(node.Status.Conditions)-1].Type)
+	fmt.Println("Node IP Address          : ",node.Status.Addresses)
+	fmt.Println("Operating System         : ",node.Status.NodeInfo.OperatingSystem)
+	fmt.Println("Hardware Architecture    : ",node.Status.NodeInfo.Architecture)
+	fmt.Println("Operating System Image   : ",node.Status.NodeInfo.OSImage)
+	fmt.Println("Operating System Kernel  : ",node.Status.NodeInfo.KernelVersion)
+	fmt.Println("Contain Runtime Version  : ",node.Status.NodeInfo.ContainerRuntimeVersion)
+	fmt.Println("Kubernetes Version       : ",node.Status.NodeInfo.KubeletVersion)
+	fmt.Println("Creation Timestamp       : ",node.CreationTimestamp)
+	fmt.Println("Last Heartbeat Timestamp : ",node.Status.Conditions[0].LastHeartbeatTime)
+	fmt.Println("CPU Capacity Kernels     : ",node.Status.Capacity.Cpu())
+	fmt.Println("Allocatable CPU Kernels  : ",node.Status.Allocatable.Cpu().String())
+	fmt.Println("Allocatable Memory Size  : ",node.Status.Allocatable.Memory().String())
+}
 
 func initClientsetConfig() (*kubernetes.Clientset,*rest.Config,error){
 	// 连接集群
@@ -36,30 +58,27 @@ func initClientsetConfig() (*kubernetes.Clientset,*rest.Config,error){
 	return clientset,config,err
 }
 
-func getNodeInfo(clientset *kubernetes.Clientset) {
+func getNodeInfo(clientset *kubernetes.Clientset,nodeName string)(*corev1.Node,error) {
 	// 获取node信息
 	nodeList, err := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		panic(err)
 	}
 	for _, node := range nodeList.Items {
-		fmt.Printf("=========================\nName    : %s\nAddress : %s\nOSImage : %s\nk8sVer  : %s\nOS      : %s\nArch    : %s\nKernel  : %s\nCreated : %s\nNowtime : %s\nCPU     : %s\nFreeCPU : %s\nDocker  : %s\nStatus  : %s\nMemory  : %s\n",
-			node.Name,
-			node.Status.Addresses,
-			node.Status.NodeInfo.OSImage,
-			node.Status.NodeInfo.KubeletVersion,
-			node.Status.NodeInfo.OperatingSystem,
-			node.Status.NodeInfo.Architecture,
-			node.Status.NodeInfo.KernelVersion,
-			node.CreationTimestamp,
-			node.Status.Conditions[0].LastHeartbeatTime,
-			node.Status.Capacity.Cpu(),
-			node.Status.Allocatable.Cpu().String(),
-			node.Status.NodeInfo.ContainerRuntimeVersion,
-			node.Status.Conditions[len(node.Status.Conditions)-1].Type,
-			node.Status.Allocatable.Memory().String(),
-		)
+		if node.Name == nodeName {
+			return &node,err
+		}
 	}
+	return nil,err
+}
+
+func getNodeListInfo(clientset *kubernetes.Clientset)(*corev1.NodeList,error) {
+	// 获取node信息
+        nodeList, err := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+        if err != nil {
+                panic(err)
+        }
+	return nodeList,err
 }
 
 func main() {
@@ -68,5 +87,10 @@ func main() {
 		panic(err.Error())
 		return
 	}
-	getNodeInfo(clientset)
+	node,_ := getNodeInfo(clientset,"master")
+	printNodeInfo(node)
+	nodeList,_ := getNodeListInfo(clientset)
+	for _,n := range nodeList.Items {
+		printNodeInfo(&n)
+	}
 }
